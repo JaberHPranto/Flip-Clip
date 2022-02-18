51c95d8de0cbe2e76a5ad81c266d3960bbbf1c84
JaberHPranto <jhpranto53@gmail.com>
2022-02-18 23:02:43 +0600

    ⚡️ update form card's css

--- content.js
+++ content.js
@@ -17,14 +17,16 @@ $(document).ready(function () {
     iFrame.id = "newCardFrame";
     iFrame.style.height = "400px";
     iFrame.style.width = "420px";
-    iFrame.style.backgroundColor = "skyblue";
     iFrame.style.position = "fixed";
-    iFrame.style.margin = "auto auto";
+    iFrame.style.margin = "auto";
     iFrame.style.zIndex = "999999";
+    iFrame.style.borderWidth = 0;
+    iFrame.style.borderRadius = "8px";
     iFrame.style.top = "0";
     iFrame.style.left = "0";
     iFrame.style.right = "0";
-    iFrame.style.bottom = "50";
+    iFrame.style.bottom = "0";
+    iFrame.setAttribute("align", "middle");
 
     document.body.insertBefore(iFrame, document.body.firstChild);
   }
