a776d2751683d81eddd6b41a934535327e39767e
JaberHPranto <jhpranto53@gmail.com>
2022-02-23 17:04:10 +0600

    🏆 set and get cards from chrome local storage

--- newCardModal.js
+++ newCardModal.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  let pageUrl;
   $("#newCardBtn").attr("disabled", true);
   // creating new card
   chrome.storage.sync.get(["message"], function (result) {
@@ -12,9 +13,8 @@ $(document).ready(function () {
       else if (result.message.itemId === "cardBack") {
         $("#answer").val(sanitizeString(result.message.text));
       }
+      pageUrl = result.message.pageUrl;
     }
-
-    // disable button until all text field is not empty
   });
 
   function checkStatus() {
@@ -38,9 +38,53 @@ $(document).ready(function () {
     });
   });
 
+  // handle card creation
+  $("#newCardBtn").click(function () {
+    getFromStorage(function (cards) {
+      console.log(cards);
+      const question = sanitizeString($("#question").val());
+      const answer = sanitizeString($("#answer").val());
+      const card = {
+        id: getID(),
+        question,
+        answer,
+        pageUrl,
+      };
+
+      cards.push(card);
+      saveToStorage(cards);
+    });
+  });
+
+  // local storage
+  function getFromStorage(callback) {
+    chrome.storage.sync.get(["cards"], function (result) {
+      if (result && result.cards) {
+        callback(result.cards);
+      } else callback([]);
+    });
+  }
+
+  function saveToStorage(cards, cb) {
+    chrome.storage.sync.set({ cards }, function () {
+      console.log("card added");
+    });
+  }
+
   // sanitizing input
   function sanitizeString(str) {
     str = str.replace(/[^a-z0-9 \.,_-]/gim, "");
     return str.trim();
   }
+
+  // generate unique id
+  function getID() {
+    console.log(Math.random().toString(36));
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
+
+  // just for testing -- will remove later
+  getFromStorage(function (cards) {
+    console.log(cards);
+  });
 });
